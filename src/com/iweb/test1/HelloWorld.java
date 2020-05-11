bb5da93dadefaef2b6e742befc319e4ffa8548f8
yuanzhaoweig <228963531@qq.com>
2020-05-11 14:18:20 +0800

    test

--- src/com/iweb/test1/HelloWorld.java
+++ src/com/iweb/test1/HelloWorld.java
@@ -3,5 +3,6 @@ package com.iweb.test1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("啦啦啦啦啦");
     }
 }
